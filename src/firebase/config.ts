e164617143120f9bbffa181b2c62723f70a0c6ab
장프로 <jangseik14@gmail.com>
2026-02-13 05:15:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5290180250-baff5",
+  "appId": "1:341639946520:web:db275fbc56d36d65f2be6d",
+  "apiKey": "AIzaSyB7bzTJ_PCXNj4Q1wdoN_SScm8G0IY3bB4",
+  "authDomain": "studio-5290180250-baff5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "341639946520"
+};
